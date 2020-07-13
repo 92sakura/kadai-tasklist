7d8fb839a99e021bb9e491eceac593d4b2879ec6
92sakura <kuniko0519@gmail.com>
2020-07-13 21:52:23 +0900

    Modify null control

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -43,8 +43,11 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("message", m);
         request.setAttribute("_token", request.getSession().getId());
 
-        // メッセージIDをセッションスコープに登録
-        request.getSession().setAttribute("message_id", m.getId());
+        // データが存在しているときのみ
+        // IDをセッションスコープに登録
+        if(m != null) {
+            request.getSession().setAttribute("message_id", m.getId());
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
